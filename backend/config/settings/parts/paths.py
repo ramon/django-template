f6b8447138f80613c60cd96e0679a19427bb30f8
Ramon Soares <ramongsoares@gmail.com>
2026-05-30 15:08:53 -0300

    Update `BASE_DIR` and `ENV_FILE` path logic in settings

--- backend/config/settings/parts/paths.py
+++ backend/config/settings/parts/paths.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[3]
 
-ENV_FILE = BASE_DIR / ".env"
+ENV_FILE = BASE_DIR.parent / ".env"
 
 __all__ = [
     "BASE_DIR",
