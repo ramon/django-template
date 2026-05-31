209c31f2c98fd4cbda735ea2bd50145ccfe7e583
Ramon Soares <ramongsoares@gmail.com>
2026-05-31 18:38:11 -0300

    Update `ENV_FILE` path to align with `BASE_DIR` structure

--- config/settings/parts/paths.py
+++ config/settings/parts/paths.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[3]
 
-ENV_FILE = BASE_DIR.parent / ".env"
+ENV_FILE = BASE_DIR / ".env"
 
 STATIC_DIR = BASE_DIR / "static"
 PUBLIC_DIR = BASE_DIR / "public"
