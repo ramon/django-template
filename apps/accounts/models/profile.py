4249e5c6c062343eee028eef6a314fd43b81714d
Ramon Soares <ramongsoares@gmail.com>
2026-05-31 18:37:48 -0300

    Enhance `Profile` model: switch to `AUTH_USER_MODEL`, add `ProfileManager` for query optimization, and define `__str__` method.

--- apps/accounts/models/profile.py
+++ apps/accounts/models/profile.py
@@ -4,7 +4,7 @@ from PIL.SpiderImagePlugin import TYPE_CHECKING
 from apps.accounts.models.mixins import AvatarMixin
 from apps.core.models import AbstractBaseModel, AbstractSortableModel
 from apps.core.models.mixins import PhoneNumberMixin
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 from django.utils.translation import pgettext_lazy
 
@@ -12,6 +12,10 @@ if TYPE_CHECKING:
     from apps.accounts.models.user import User
     from apps.accounts.models.gender import Gender
 
+class ProfileManager(models.Manager):
+    def get_queryset(self):
+        return (super().get_queryset()
+                .select_related('user', 'gender'))
 
 
 class Profile(AvatarMixin,
@@ -36,13 +40,15 @@ class Profile(AvatarMixin,
             instance, protected against deletion of associated Gender entries.
     """
     user: "User" = models.OneToOneField(
-        get_user_model(),
+        settings.AUTH_USER_MODEL,
         related_name="profile",
         on_delete=models.CASCADE
     )
     document: str = models.CharField(max_length=11, null=True, blank=True, unique=True)
     birth_date: datetime.date = models.DateField(blank=True, null=True)
     gender: "Gender" = models.ForeignKey('accounts.Gender', blank=True, null=True, on_delete=models.PROTECT)
+
+    objects = ProfileManager()
 
     class Meta:
         verbose_name = pgettext_lazy('model', 'profile')
@@ -51,3 +57,5 @@ class Profile(AvatarMixin,
             models.Index(fields=['document'], name='idx_profile_document'),
         ]
 
+    def __str__(self):
+        return self.user.name.full
