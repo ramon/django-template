c0b865bbdb2729e68975d32576536291422bb60c
Ramon Soares <ramongsoares@gmail.com>
2026-05-31 20:20:50 -0300

    Enable `debug_toolbar`: update settings to add middleware, app, and suppress `debug_toolbar.W006`.

--- config/settings/parts/debug.py
+++ config/settings/parts/debug.py
@@ -1,6 +1,6 @@
 from .django import SILENCED_SYSTEM_CHECKS, INSTALLED_APPS, MIDDLEWARE
 
-SILENCED_SYSTEM_CHECKS.append("security.W006")
+SILENCED_SYSTEM_CHECKS.append("debug_toolbar.W006")
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
 INSTALLED_APPS += ["debug_toolbar"]
 MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
